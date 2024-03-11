9712e2db36c1ba55a8d6428a6ac850a5e55f503b
Robert Overs <oversr14@gmail.com>
2024-03-11 15:32:26 +1100

    Formatting

--- lists/lists.pl
+++ lists/lists.pl
@@ -1,60 +1,52 @@
-% Problem 1
+% Problem 1 (*)
 % Find the last element of a list.
 
 my_last([L], L).
-my_last([_|T], L) :-
-  my_last(T, L).
+my_last([_|Rest], L) :-
+  my_last(Rest, L).
 
 
-% Problem 2
+% Problem 2 (*)
 % Find the last-but-one (or second-last) element of a list.
 
 my_but_last([K,_], K). 
-my_but_last([_|T], K) :-
-  my_but_last(T, K).
+my_but_last([_|Rest], K) :-
+  my_but_last(Rest, K).
 
 
-% Problem 3
+% Problem 3 (*)
 % Find the Kth element of a list.
 
 element_at([H|_], 1, H).
-element_at([_|T], K, H) :-
+element_at([_|Rest], K, H) :-
   Ki is K-1,
-  element_at(T, Ki, H).
+  element_at(Rest, Ki, H).
 
 
-% Problem 4
+% Problem 4 (*)
 % Find the number of elements in a list.
 
 my_length([], 0).
-my_length([_|T], N) :-
-  my_length(T, Ni),
+my_length([_|Rest], N) :-
+  my_length(Rest, Ni),
   N is Ni+1.
 
-% tail-recursive version
-my_length_tr(List, N) :-
-  my_length_aux(List, 0, N).
-my_length_aux([], N, N).
-my_length_aux([_|T], I, N) :-
-  Ii is I+1,
-  my_length_aux(T, Ii, N).
 
-
-% Problem 5
+% Problem 5 (*)
 % Reverse a list.
 
 my_reverse(List, Reversed) :-
   my_reverse_aux(List, [], Reversed).
 my_reverse_aux([], Acc, Acc).
-my_reverse_aux([H|T], Acc, Reversed) :-
-  my_reverse_aux(T, [H|Acc], Reversed).
+my_reverse_aux([H|Rest], Acc, Reversed) :-
+  my_reverse_aux(Rest, [H|Acc], Reversed).
 
 
-% Problem 6
+% Problem 6 (*)
 % Find out whether a list is a palindrome.
 
 is_palindrome([]).
 is_palindrome([_]).
 is_palindrome(List) :-
-  append([H|T], [H], List),
-  is_palindrome(T).
+  append([H|Mid], [H], List),
+  is_palindrome(Mid).
