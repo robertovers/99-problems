37f9c5c840fc2f3a4f1065ef89dfc1f3ef5e1774
Robert Overs <oversr14@gmail.com>
2024-04-06 19:24:48 +1100

    Haskell problems 5,6

--- lists/lists.hs
+++ lists/lists.hs
@@ -1,4 +1,4 @@
--- Problem 1
+-- Problem 1 (*)
 -- Find the last element of a list.
 
 myLast :: [a] -> a
@@ -6,7 +6,7 @@ myLast [a] = a
 myLast (x:xs) = myLast xs
 
 
--- Problem 2
+-- Problem 2 (*)
 -- Find the last-but-one (or second-last) element of a list.
 
 myButLast :: [a] -> a
@@ -14,7 +14,7 @@ myButLast [a, b] = a
 myButLast (x:xs) = myButLast xs
 
 
--- Problem 3
+-- Problem 3 (*)
 -- Find the Kth element of a list.
 
 elementAt :: [a] -> Int -> a
@@ -22,15 +22,33 @@ elementAt (x:xs) 1 = x
 elementAt (x:xs) k = elementAt xs (k-1)
 
 
--- Problem 4
+-- Problem 4 (*)
 -- Find the number of elements in a list.
 
 myLength :: [a] -> Int
-myLength = foldr (\ x -> (+) 1) 0
+myLength = foldr (\x -> (+) 1) 0
 
+
+-- Problem 5 (*)
+-- Reverse a list.
+
+myReverse :: [a] -> [a]
+myReverse list = myReverseAux list []
+
+myReverseAux :: [a] -> [a] -> [a]
+myReverseAux [] acc = acc
+myReverseAux (x:xs) acc = myReverseAux xs (x:acc)
+
+
+-- Problem 6 (*)
+-- Find out whether a list is a palindrome.
+
+isPalindrome :: Eq a => [a] -> Bool
+isPalindrome [] = True
+isPalindrome list = myReverse list == list
 
 
 main :: IO ()
 main = do
-  let out = myLength [1, 2, 3, 4]
+  let out = isPalindrome [1, 2, 3, 2, 1]
   print out
